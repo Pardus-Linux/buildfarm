6e7fb6082a574122b7162ee5becd2514b428949a
A. Murat Eren <meren@pardus.org.tr>
2006-03-10 14:28:08 +0000

    a'dan iki tane binary çıksın..

--- exampleRepo/a/actions.py
+++ exampleRepo/a/actions.py
@@ -8,9 +8,13 @@
 # A. Murat Eren <meren at uludag.org.tr>
 
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
 WorkDir = "merhaba-pisi-1.0"
 
 def install():
     pisitools.dobin("merhaba-pisi.py")
     pisitools.rename("/usr/bin/merhaba-pisi.py", "merhaba-pisi")
+    pisitools.dodir("/usr/lib")
+    shelltools.copy(get.installDIR() + "/usr/bin/merhaba-pisi", get.installDIR() + "/usr/lib/merhaba-osman")
