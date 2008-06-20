b9c2932267c518719adc040a765a1153e553bb06
Ozan Çağlayan <ozan@pardus.org.tr>
2008-06-20 13:28:10 +0000

    remove unnecessary passes

--- main.py
+++ main.py
@@ -114,10 +114,8 @@ def buildPackages():
     logger.info(_("Wait Queue: %s") % (qmgr.waitQueue))
     if qmgr.waitQueue:
         mailer.info(_("Queue finished with problems and those packages couldn't be compiled:\n\n%s\n\n\nNew binary packages are;\n\n%s\n\nnow in repository") % ("\n".join(qmgr.waitQueue), "\n".join(packageList)))
-        pass
     else:
         mailer.info(_("Queue finished without a problem!...\n\n\nNew binary packages are;\n\n%s\n\nnow in repository...") % "\n".join(packageList))
-        pass
     logger.raw()
 
     logger.raw()
