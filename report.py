f51805c52085205c759c5b19a6ef9e32824d6de1
Ozan Çağlayan <ozan@pardus.org.tr>
2008-08-12 08:47:07 +0000

    no need to gettext..

--- report.py
+++ report.py
@@ -4,15 +4,10 @@
 import os
 import mailer
 
-""" Gettext Support """
-import gettext
-__trans = gettext.translation("buildfarm", fallback = True)
-_  =  __trans.ugettext
-
 packages = []
 for i in os.listdir("/var/www/localhost/htdocs/pardus-2007-test/"):
     if not os.path.exists("/var/www/localhost/htdocs/pardus-2007/%s" % i):
         packages.append(i)
 
 packages.sort()
-mailer.announce(_("New packages in -testing repository:\n\n%s") % "\n".join(packages))
+mailer.announce("New packages in -testing repository:\n\n%s" % "\n".join(packages))
