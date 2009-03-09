d56a63ee2c49e3925e31e2719a9ecd6689fa1434
Ozan Çağlayan <ozan@pardus.org.tr>
2009-03-09 10:16:37 +0000

    Update config

--- config.py
+++ config.py
@@ -16,40 +16,48 @@ import os
 #
 # Workdir
 workDir          = "/var/pisi/"
+
 # Local source repo
 localPspecRepo   = "/root/2008"
+
 # Main buildfarm log
 logFile          = "/var/cache/pisi/buildfarm.log"
+
 # All package logs are kept in this directory as
 # package_name-log.
 outputDir        = "/var/cache/pisi/buildlogs/"
+
 # This repo contains all binaries and deltas built
 binaryPath       = "/var/cache/pisi/packages/"
+
 # This repo contains latest binaries and 3 deltas -
 # one delta from iso release to latest binary, two deltas
 # from previus releases.
 testPath         = "/var/cache/pisi/packages-test/"
+
 # This repo contains only the latest binaries and deltas.
 deltaPath        = "/var/cache/pisi/packages-delta/"
 
 # Information for mailer module.
-#
 # Edit templates.py for the  structure of mails.
-#
-# Default from adress
+
+# Default from address
 mailFrom         = "buildfarm@pardus.org.tr"
-# This adress is used for automated reports
+
+# This address is used for automated reports
 announceAddr     = "buildfarm@pardus.org.tr"
-# This is the default CC adress for all info and error mails
-# Error mails are sent to package maintainer and CC'ed to this adress,
-# info mails have no 'TO' adress, only this one as a 'CC'.
+
+# This is the default CC address for all info and error mails
+# Error mails are sent to package maintainer and CC'ed to this address,
+# info mails have no 'TO' address, only this one as a 'CC'.
 ccList           = ["buildfarm@pardus.org.tr"]
+
 # Server for sending mail.
 smtpServer       = "mail.pardus.org.tr"
+
 # Create and edit mailauth.py for authentication if needed
 # Set this to 'False' for turning mail sending off.
 useSmtpAuth      = False
-
 
 # Blacklist for delta packages. Buildfarm will never build
 # delta packages for them.
