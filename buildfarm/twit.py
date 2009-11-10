a77a8cd265568aef12938af04def87c43acfbbb7
Ozan Çağlayan <ozan@pardus.org.tr>
2009-11-10 12:16:40 +0000

    Add new transparent Twitter class to send buildfarm updates

---
+++ buildfarm/twit.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2009 TUBITAK/UEKAE
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Please read the COPYING file.
+
+class Twitter(object):
+    def __init__(self):
+        self.api = None
+        try:
+            import twitter
+        except ImportError:
+            pass
+        else:
+            # Read the credentials from a safe place
+            self.api = twitter.Api('', '')
+
+    def update(self, msg):
+        if self.api:
+            return self.api.PostUpdate(msg)
+
+
+if __name__ == "__main__":
+    t = Twitter()
+    t.update("Test")
