6ae970254ef49cbec6efd41d774bd38bd62850ca
Ozan Çağlayan <ozan@pardus.org.tr>
2010-10-13 06:51:38 +0000

    add pylint tester

---
+++ test/pylint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# C0111: Ignore missing docstrings
+PYLINT="pylint -d C0111"
+
+for module in $(ls buildfarm/*.py); do
+    $PYLINT $module 2>/dev/null > buildfarm/`basename $module`.pylint
+done
+
+for script in $(ls scripts); do
+    $PYLINT $script 2>/dev/null > scripts/`basename $script`.pylint
+done
